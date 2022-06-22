5b9d07a6aaf4a89975319e36aa542fb0f0fecf7b
Maneesh-Arora <maneesh2000@yahoo.com>
2022-06-22 10:26:06 +0530

    Implement Vendor

--- src/main/java/guru/springfamework/bootstrap/Bootstrap.java
+++ src/main/java/guru/springfamework/bootstrap/Bootstrap.java
@@ -2,8 +2,10 @@ package guru.springfamework.bootstrap;
 
 import guru.springfamework.domain.Category;
 import guru.springfamework.domain.Customer;
+import guru.springfamework.domain.Vendor;
 import guru.springfamework.repositories.CategoryRepository;
 import guru.springfamework.repositories.CustomerRepository;
+import guru.springfamework.repositories.VendorRepository;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
@@ -11,18 +13,32 @@ import org.springframework.stereotype.Component;
 public class Bootstrap implements CommandLineRunner
 {
 
-    private CategoryRepository categoryRepository;
-    private CustomerRepository customerRepository;
+    private final CategoryRepository categoryRepository;
+    private final CustomerRepository customerRepository;
+    private final VendorRepository vendorRepository;
 
-    public Bootstrap(CategoryRepository categoryRepository, CustomerRepository customerRepository)
+    public Bootstrap(CategoryRepository categoryRepository, CustomerRepository customerRepository,VendorRepository vendorRepository)
     {
         this.categoryRepository = categoryRepository;
         this.customerRepository = customerRepository;
+        this.vendorRepository = vendorRepository;
     }
     @Override
     public void run(String... args) throws Exception {
         loadCategories();
-        loadCustomer();
+        loadCustomers();
+        loadVendors();
+    }
+
+    private void loadVendors() {
+        Vendor vendor1 = new Vendor();
+        vendor1.setName("Vendor 1");
+        vendorRepository.save(vendor1);
+
+        Vendor vendor2 = new Vendor();
+        vendor2.setName("Vendor 2");
+        vendorRepository.save(vendor2);
+
     }
 
     private void loadCategories() {
@@ -50,13 +66,21 @@ public class Bootstrap implements CommandLineRunner
         System.out.println("Data Loaded = " + categoryRepository.count() );
     }
 
-    private void loadCustomer() {
-        Customer susan = new Customer();
-        susan.setFirstName("Susan");
-        susan.setLastName("Tanner");
+    private void loadCustomers() {
+        //given
+        Customer customer1 = new Customer();
+        customer1.setId(1l);
+        customer1.setFirstName("Michale");
+        customer1.setLastName("Weston");
+        customerRepository.save(customer1);
 
-        customerRepository.save(susan);
+        Customer customer2 = new Customer();
+        customer2.setId(2l);
+        customer2.setFirstName("Sam");
+        customer2.setLastName("Axe");
 
-        System.out.println("Customer dat Loaded = " + customerRepository.count());
+        customerRepository.save(customer2);
+
+        System.out.println("Customers Loaded: " + customerRepository.count());
     }
 }
